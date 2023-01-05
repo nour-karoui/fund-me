68a2f44d5447d417e533720653b8c67cecd85b1c
AliMarzouk <ali.marzouk2@gmail.com>
2023-01-05 22:47:38 +0100

    add type null safety checker

--- crowdfund/src/components/projects/projectsList.tsx
+++ crowdfund/src/components/projects/projectsList.tsx
@@ -8,27 +8,22 @@ import { SearchIcon } from '@chakra-ui/icons';
 
 export const ProjectsList: React.FC<{projectsFactory: any, handleProjectChange: any}> = ({projectsFactory, handleProjectChange}) => {
 
-    const [avaiblableProjects, setAvailableProjects] = React.useState<string[]|null>(null);
-    // const projects = [
-    //     {name: 'Webipie'},
-    //     {name: 'Ariana'},
-    //     {name: 'Buy me a coffee'},
-    //     {name: 'Shopify'},
-    //     {name: 'Polkadot'},
-    //     {name: 'Cardano'},
-    // ]
+    const [availableProjects, setAvailableProjects] = React.useState<string[]|null>(null);
+
     useEffect(() => {
         fetchAvailableProjects();
-    });
+    }, [projectsFactory]);
     const fetchAvailableProjects = async () => {
-        const response = await projectsFactory.projectsCount();
-        const projectsCount = response.toNumber();
-        let projects = [];
-        for(let i = 0; i<projectsCount; i++) {
-            const projectName = await projectsFactory.projectsNames(i);
-            projects.push(projectName);
+        if (projectsFactory) {
+            const response = await projectsFactory.projectsCount();
+            const projectsCount = response.toNumber();
+            let projects = [];
+            for(let i = 0; i < projectsCount; i++) {
+                const projectName = await projectsFactory.projectsNames(i);
+                projects.push(projectName);
+            }
+            setAvailableProjects(projects);
         }
-        setAvailableProjects(projects);    
     }
 
     return (
@@ -42,7 +37,7 @@ export const ProjectsList: React.FC<{projectsFactory: any, handleProjectChange: 
                 <Input placeholder='Project Name' />
                 <InputRightElement children={<SearchIcon color='teal.500' />} />
             </InputGroup>
-            {avaiblableProjects?.map((project, i) => <Box onClick={() => handleProjectChange(project)} key={i} borderRadius='lg' p={4} _hover={{ color: "#F8F4EA", background: "#439A97", fontWeight: 'semibold', cursor: "pointer"}}>{project}</Box>)}
+            {availableProjects?.map((project, i) => <Box onClick={() => handleProjectChange(project)} key={i} borderRadius='lg' p={4} _hover={{ color: "#F8F4EA", background: "#439A97", fontWeight: 'semibold', cursor: "pointer"}}>{project}</Box>)}
         </Box>
         // <Center mt={3} pt={10}>
         //     <FormControl isRequired>
